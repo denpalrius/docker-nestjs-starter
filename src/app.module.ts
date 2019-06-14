0f17145134c898447ea2d2b5cd23eeee57fb1710
Dennis Riungu <denpalrius@gmail.com>
2019-06-14 19:59:41 +0300

    Add node redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { TerminusModule } from '@nestjs/terminus';
 import { TerminusOptionsService } from './terminus-options.service';
 import { DogHealthIndicatorService } from './dog.health';
+import { RedisModule } from 'nestjs-redis';
 
 @Module({
   imports: [
+    // RedisModule.register({
+    //   host: process.env.REDIS_HOST,
+    //   port: parseInt(process.env.REDIS_PORT),
+    //   db: parseInt(process.env.REDIS_DB),
+    //   password: process.env.REDIS_PASSWORD,
+    //   keyPrefix: process.env.REDIS_PRIFIX,
+    // } as any),
     TerminusModule.forRootAsync({
       useClass: TerminusOptionsService,
     }),
