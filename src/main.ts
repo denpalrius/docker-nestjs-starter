9f88e4e181ef2a8c3409a3cac477b83f6aa4265e
Dennis Riungu <denpalrius@live.com>
2020-05-29 00:45:46 +0300

    Terminate app gracefully

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let server: { close: (arg0: (err: any) => void) => void };
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  server = await app.listen(3000);
+
+  // Handle process kill signals
+  process.on('SIGINT', shutdown);
+  process.on('SIGTERM', shutdown);
 }
+
+function shutdown() {
+  // Gracefully close outstanding HTTP connections
+  server.close((err) => {
+    if (err) {
+      console.error('An error occurred while closing the server. Forecefullly shutting down');
+      console.error(err);
+      process.exit(1);
+    }
+    console.log('Http server closed.');
+
+    // Close data connections here, eg database pool connections
+
+    // clean up your resources and exit
+    process.exit(0);
+  });
+}
+
 bootstrap();
